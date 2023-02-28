7e8a2dc6116a1a39231dab1a92ff6cd69b4e67d3
Glitch (lab06-webapp-2023) <none>
2023-02-28 12:50:38 +0000

    🗽🔅 Updated with Glitch

--- models/playlist-store.js
+++ models/playlist-store.js
@@ -8,39 +8,32 @@ const playlistStore = {
   store: new JsonStore('./models/playlist-store.json', { playlistCollection: [] }),
   collection: 'playlistCollection',
 
-  //updated
   getAllPlaylists() {
     return this.store.findAll(this.collection);
   },
   
-  //updated
   getPlaylist(id) {
     return this.store.findOneBy(this.collection, (collection => collection.id === id));
   },
   
-  //updated
   removeSong(id, songId) {
     const arrayName = "songs";
     this.store.removeItem(this.collection, id, arrayName, songId);
   },
   
-  //updated
   removePlaylist(id) {
     const playlist = this.getPlaylist(id);
     this.store.removeCollection(this.collection, playlist);
   },
   
-  //added
   removeAllPlaylists() {
     this.store.removeAll(this.collection);
   },
   
-  //updated
   addPlaylist(playlist) {
     this.store.addCollection(this.collection, playlist);
   },
   
-  //updated
   addSong(id, song) {
     const arrayName = "songs";
     this.store.addItem(this.collection, id, arrayName, song);
